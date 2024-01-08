a750bc4d62e6e08290f918bedec6804fb6f53683
Yu Kamiya <fuzzy31u@gmail.com>
2024-01-08 10:56:27 +0900

    add: white and black simple pattern

---
+++ main.js
@@ -0,0 +1,13 @@
+$(function() {
+  var navbar = $('.navbar');
+  var navbarHeight = navbar.outerHeight(true);
+  $(window).on('scroll', function() {
+      if ($(this).scrollTop() > $('header').height() - navbarHeight) {
+          navbar.addClass('fixed-top');
+          $('body').css('padding-top', navbarHeight);
+      } else {
+          navbar.removeClass('fixed-top');
+          $('body').css('padding-top', '0');
+      }
+  });
+});
